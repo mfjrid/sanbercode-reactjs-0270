083d7f76c91993c5820c1a90df7898fc6f4e25ac
MFJRID <mfjr48@gmail.com>
2020-08-02 23:44:42 +0700

    Update Tugas 10

---
+++ Pekan 2/Tugas Hari Ke-5/tugas10react/src/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+        <h1>Form Pembelian Buah</h1>
+        <table>
+          <tr>
+            <th>Nama Pelanggan</th>
+            <td>
+              <input type="text" />
+            </td>
+          </tr>
+          <tr>
+            <th></th>
+            <td>
+              <input type="checkbox" name="buah" id="semangka" />
+              Semangka
+            </td>
+          </tr>
+          <tr>
+            <th></th>
+            <td>
+              <input type="checkbox" name="buah" id="jeruk" />
+              Jeruk
+            </td>
+          </tr>
+          <tr>
+            <th></th>
+            <td>
+              <input type="checkbox" name="buah" id="nanas" />
+              Nanas
+            </td>
+          </tr>
+          <tr>
+            <th></th>
+            <td>
+              <input type="checkbox" name="buah" id="salak" />
+              Salak
+            </td>
+          </tr>
+          <tr>
+            <th>Daftar Item</th>
+            <td>
+              <input type="checkbox" name="buah" id="anggur" />
+              Anggur
+            </td>
+          </tr>
+          <tr>
+            <button type="submit">Kirim</button>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
